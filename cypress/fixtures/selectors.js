a13bd1260f017d91cb75318622b5c5b1ae80b98b
Adaeze Atusiuba <aadaezea@gmail.com>
2022-01-14 02:30:14 +0100

    add login page testcases

---
+++ cypress/fixtures/selectors.js
@@ -0,0 +1,11 @@
+
+export const login = {
+    emailAddressText: '[data-testid="email"]',
+    emailAddressInput:'qa@testing.com',
+    passwordText:'[data-testid="password"]',
+    passwordInput:'Password1!',
+    wrongPassword:'book',
+    keepMeIn:'.space-x-2 > .text-sm',
+    logInButton:'[data-testid="login"]'
+}
+
---
+++ cypress/integration/login.js
@@ -0,0 +1,39 @@
+
+import { login} from "../fixtures/selectors";
+
+describe("Authentication",function(){
+        beforeEach(function () {
+            cy.visit("https://sandbox.fincra.com/auth/login")
+        });
+
+    it("validLogin",()=>{
+                cy.get(login.emailAddressText).should('be.visible').clear().type(login.emailAddressInput)
+                cy.get(login.passwordText).should('be.visible').clear().type(login.passwordInput)
+                cy.get(login.logInButton).click()
+                cy.wait(3000)
+                
+             })
+
+    it("InvalidLogin",()=>{
+                cy.get(login.emailAddressText).should('be.visible').clear().type(login.emailAddressInput)
+                cy.get(login.passwordText).should('be.visible').clear().type(login.wrongPassword)
+                cy.get(login.logInButton).click()
+                cy.wait(3000)
+                
+             })
+
+    it.only("complete login",()=>{
+
+                cy.get(login.emailAddressText).should('be.visible').clear().type(login.emailAddressInput)
+                cy.get(login.passwordText).should('be.visible').clear().type(login.passwordInput)
+                cy.get(login.keepMeIn).click()
+                cy.get(login.logInButton).click()
+                cy.wait(3000)
+
+    } )
+
+
+
+
+
+})
